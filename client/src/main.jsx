7dd80b057a4bd1614dccfce90c338bacff10ebfd
Diego <odiegodev@gmail.com>
2024-10-21 21:14:45 -0300

    chore: Instalei sass e o react router dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/aff',
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
